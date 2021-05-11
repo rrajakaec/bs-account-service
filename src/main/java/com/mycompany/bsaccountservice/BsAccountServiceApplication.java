aedb8f14a03ce814f4014f94c0ca5c911c9dc2c5
Rintu Rajak <rintu.rajak@gmail.com>
2021-05-11 18:11:54 +0530

    added initial version

---
+++ src/main/java/com/mycompany/bsaccountservice/BsAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.bsaccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsAccountServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompany/bsaccountservice/BsAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.bsaccountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsAccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
